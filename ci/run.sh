bf682d4cf238737fc14a0f73f053062d04e67679
Uggla <uggla@gree.fr>
2018-11-19 17:17:43 +0000

    Open socket

--- ci/run.sh
+++ ci/run.sh
@@ -3,5 +3,7 @@
 # Create a Jenkins group and user
 groupadd -g 1000 jenkins
 useradd -k /etc/skel/ -m -u 1000 -g 1000 -G docker -r -s /bin/bash jenkins
+# Open the docker socker widely (do not do this on production system
+chmod o+rw /var/run/docker.sock
 
 docker-compose up -d
