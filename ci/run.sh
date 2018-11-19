afb89a3bd6035f7e8ba26b51bdc4b96caaa44ea9
Uggla <uggla@gree.fr>
2018-11-19 17:02:48 +0000

    Allow jenkins to use the Docker unix socket

--- ci/run.sh
+++ ci/run.sh
@@ -2,6 +2,6 @@
 
 # Create a Jenkins group and user
 groupadd -g 1000 jenkins
-useradd -k /etc/skel/ -m -u 1000 -g 1000 -r -s /bin/bash jenkins
+useradd -k /etc/skel/ -m -u 1000 -g 1000 -G docker -r -s /bin/bash jenkins
 
 docker-compose up -d
