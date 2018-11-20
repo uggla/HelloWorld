d4e95d1a94dea054caeb68dd099902defae25698
Uggla <uggla@gree.fr>
2018-11-20 10:03:18 +0000

    Update socket after Docker restart

--- ci/run.sh
+++ ci/run.sh
@@ -5,12 +5,13 @@ REGISTRY_FQDN=registry.uggla.fr
 # Create a Jenkins group and user
 groupadd -g 1000 jenkins
 useradd -k /etc/skel/ -m -u 1000 -g 1000 -G docker -r -s /bin/bash jenkins
-# Open the docker socker widely (do not do this on production system
-chmod o+rw /var/run/docker.sock
 
 # Get certificate from registry
 curl -L http://${REGISTRY_FQDN}:81/ca.crt > /usr/local/share/ca-certificates/ca-registry.crt
 update-ca-certificates
 systemctl restart docker.service
+
+# Open the docker socker widely (do not do this on production system
+chmod o+rw /var/run/docker.sock
 
 docker-compose up -d
