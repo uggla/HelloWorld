4af888607ca2791787c813aaffdfb12ccaedac8d
Uggla <uggla@free.fr>
2019-10-09 03:00:11 +0200

    update

--- myMethod.groovy
+++ myMethod.groovy
@@ -7,18 +7,16 @@ def pushFileToGit(String file) {
     sh 'git config --global user.name "Uggla"'
     sh 'git checkout ci_debug'
     sh "git add ${file}"
-    def result
     try {
-        result = sh 'git commit -m "new machin"'
+        sh 'git commit -m "new machin"'
+        withCredentials([sshUserPrivateKey(credentialsId: 'uggla', keyFileVariable: 'pkey', passphraseVariable: 'passphrase', usernameVariable: 'user')]) {
+            sh 'git config core.sshCommand "ssh -i ${pkey} -F /dev/null -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"'
+            sh 'git remote set-url origin git@github.com:uggla/HelloWorld.git'
+            sh(script: 'git push', returnStdout:true)
+        }
     }
     catch (Exception ex){
-        println("Here")
-        println(result)
-    }
-    withCredentials([sshUserPrivateKey(credentialsId: 'uggla', keyFileVariable: 'pkey', passphraseVariable: 'passphrase', usernameVariable: 'user')]) {
-        sh 'git config core.sshCommand "ssh -i ${pkey} -F /dev/null -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"'
-        sh 'git remote set-url origin git@github.com:uggla/HelloWorld.git'
-        sh(script: 'git push', returnStdout:true)
+        println("Nothing to commit")
     }
 }
 
