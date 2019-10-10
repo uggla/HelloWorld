2b4fa4203afb04fde094972114edbb584848e3ad
Uggla <uggla@free.fr>
2019-10-10 23:51:30 +0200

    update

--- revision.groovy
+++ revision.groovy
@@ -1,41 +1,41 @@
-class buildHistory {
+// class buildHistory {
 
-    private String historyFile
-    private String currentBuild
-    private String currentCommit
-    private String prevBuild
-    private String prevCommit
-    private def data = [:]
+//     private String historyFile
+//     private String currentBuild
+//     private String currentCommit
+//     private String prevBuild
+//     private String prevCommit
+//     private def data = [:]
 
-    buildHistory(String historyFile, String currentCommit, String currentBuild){
-        this.historyFile = historyFile
-        this.currentCommit = currentCommit
-        this.currentBuild = currentBuild
-        this.readHistory()
-        this.currentBuild = this.data.max { it.keys }
-        this.currentCommit = this.data.max { it.keys }.value
-    }
+//     buildHistory(String historyFile, String currentCommit, String currentBuild){
+//         this.historyFile = historyFile
+//         this.currentCommit = currentCommit
+//         this.currentBuild = currentBuild
+//         this.readHistory()
+//         this.currentBuild = this.data.max { it.keys }
+//         this.currentCommit = this.data.max { it.keys }.value
+//     }
 
-    void readHistory() {
-        if ( fileExists(this.historyFile)) {
-            lock(resource: "lock_${this.historyFile}", inversePrecedence: true) {
-                    this.data = readJSON(file:this.historyFile)
-            }
-        }
-    }
+//     void readHistory() {
+//         if ( fileExists(this.historyFile)) {
+//             lock(resource: "lock_${this.historyFile}", inversePrecedence: true) {
+//                     this.data = readJSON(file:this.historyFile)
+//             }
+//         }
+//     }
 
 
-    void writeHistory() {
-        lock(resource: "lock_${this.historyFile}", inversePrecedence: true) {
-            writeJSON(file:this.historyFile, json:this.data)
-        }
-    }
+//     void writeHistory() {
+//         lock(resource: "lock_${this.historyFile}", inversePrecedence: true) {
+//             writeJSON(file:this.historyFile, json:this.data)
+//         }
+//     }
 
-    void updateHistory() {
-        this.data[currentBuild] = this.currentCommit
-        this.writeHistory()
-    }
-}
+//     void updateHistory() {
+//         this.data[currentBuild] = this.currentCommit
+//         this.writeHistory()
+//     }
+// }
 
 def build(String historyFile, String currentCommit, String currentBuild) {
     //return(new buildHistory(historyFile, currentCommit, currentBuild));
